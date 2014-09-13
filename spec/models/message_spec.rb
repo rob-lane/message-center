1b52507609fbf05eddeb64591518e59cbec02457
roblane09 <roblane09@gmail.com>
2014-09-13 15:35:06 -0700

    validation of body on message model spec using build method to avoid invalid record errors

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 RSpec.describe Message, :type => :model do
 
 
-  context "creating a message" do
+  context "building a message" do
 
-    subject { FactoryGirl.create(:message) }
+    subject { FactoryGirl.build(:message) }
 
     it { is_expected.to be_valid }
 
     # No empty body messages
     context "without a body" do
 
-      subject { FactoryGirl.create(:message, {body: nil} ) }
+      subject { FactoryGirl.build(:message, {body: nil} ) }
 
       it { is_expected.to_not be_valid }
     end
