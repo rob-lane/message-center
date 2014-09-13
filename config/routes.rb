77e7416a3b83e830a44087a10bfcf3f60a5f37e9
roblane09 <roblane09@gmail.com>
2014-09-13 15:44:21 -0700

    New API controller and routes for messages, only show and index actions at this time

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  
+  namespace :api do
+    namespace :v1 do
+      #This will be a full resource eventually
+      get 'messages/index'
+      get 'messages/show'
+  end
+
   get 'public/index'
 
   root to: 'public#index'
