077c62f21cf76e37d83cc2e98e50d23455008ae1
roblane09 <roblane09@gmail.com>
2014-09-13 16:24:28 -0700

    working specs for messages API controller

--- spec/controllers/api/v1/messages_controller_spec.rb
+++ spec/controllers/api/v1/messages_controller_spec.rb
@@ -2,19 +2,43 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::MessagesController, :type => :controller do
 
+  let!(:test_msg) { FactoryGirl.create(:message) }
+
   describe "GET 'index'" do
 
-    subject { get 'index' }
+    subject! { get 'index', format: :json }
+
+    let (:expected_json) { {messages: [test_msg] }.to_json }
 
     it { is_expected.to be_success }
+
+    it "assigns all messages" do
+      expect(assigns(:messages)).to eq([test_msg])
+    end
+
+    it "responds with a JSON body" do
+      expect(response.body).to eq(expected_json)
+    end
 
   end
 
   describe "GET 'show'" do
 
-    subject { get 'show' }
+    subject! { get 'show', id: test_msg.id, format: :json }
+
+    let (:expected_json) { test_msg.to_json }
 
     it { is_expected.to be_success }
+
+    it "assigns the correct message" do
+      expect(assigns(:message)).to eq(test_msg)
+    end
+
+    it "responds with a JSON body" do
+      expect(response.body).to eq(expected_json)
+    end
+
+
 
   end
 
