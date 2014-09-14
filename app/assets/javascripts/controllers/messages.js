1fa446f7952558c5b3817da9f5eaa70078565bf1
roblane09 <roblane09@gmail.com>
2014-09-14 01:38:49 -0700

    messages controller checks model validation before creating message messages new template displays errors on body if form is dirty (submitted at least once)

--- app/assets/javascripts/controllers/messages.js
+++ app/assets/javascripts/controllers/messages.js
@@ -3,15 +3,25 @@ MessageCenter.MessagesNewController = Ember.ObjectController.extend({
     createMessage: function() {
       var self = this;
 
+      self.set('dirtyForm', true);
+
       function transitionToMessage(message) {
         self.transitionToRoute('message', message);
       }
 
       function failure(reason) {
+        this.set('serverError', reason)
         alert('error: ' + reason);
       }
 
-      this.model.save().then(transitionToMessage).catch(failure);
-    }
+      this.model.validate().then(function() {
+        if (self.model.get('isValid')) {
+          self.model.save().then(transitionToMessage).catch(failure);
+        }
+      })
+    },
+    dirtyForm: false,
+    serverError: null
   }
+
 });
