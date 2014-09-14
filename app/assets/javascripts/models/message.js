21c65d72886510a29dbe97e0290deb9ce35879a2
roblane09 <roblane09@gmail.com>
2014-09-14 01:40:10 -0700

    adding ember-validations mixin to message model adding validations object and including body attribute

--- app/assets/javascripts/models/message.js
+++ app/assets/javascripts/models/message.js
@@ -1,6 +1,11 @@
-MessageCenter.Message = DS.Model.extend({
+MessageCenter.Message = DS.Model.extend(Ember.Validations.Mixin, {
   subject: DS.attr('string'),
   body: DS.attr('string'),
   created_at: DS.attr('string'),
-  updated_at: DS.attr('string')
+  updated_at: DS.attr('string'),
+  validations: {
+    body: {
+      presence: { message: 'A message body is required...' }
+    }
+  }
 });
