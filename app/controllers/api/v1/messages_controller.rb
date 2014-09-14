1f4851be2c2827d7898de1d26330d64ade565bbc
r.lane <r.lane@fluidappsdev.com>
2014-09-14 15:54:39 -0700

    collect_recip_ids updated to read easier and running in before_action, only for create action

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::MessagesController < Api::V1::BaseController
   rescue_from ActiveRecord::RecordNotFound, with: :render_exception
+  before_action :collect_recip_ids!, only: :create
 
   def index
     @messages = Message.order('created_at DESC')
@@ -13,8 +14,6 @@ class Api::V1::MessagesController < Api::V1::BaseController
 
   def create
     render_exception('Missing recipients') unless params[:message][:recipients]
-
-    collect_recip_ids!
 
     @message = Message.create(message_params)
     if @message && @message.valid?
@@ -30,15 +29,17 @@ class Api::V1::MessagesController < Api::V1::BaseController
       params.require(:message).permit :subject, :body, recipient_ids: []
     end
 
-    def contact_params
+    def contact_params(email)
+      params[:message][:contact] = { email: email }
       params.require(:message).require(:contact).permit :email
     end
 
     def collect_recip_ids!
-      recip_ids = params[:message][:recipients].
-          map do |recip|
-        (Contact.find_by_email(recip) || Contact.create(contact_params)).id
+      params[:message][:recipient_ids] = []
+      params[:message][:recipients].each do |email|
+        existing = Contact.find_by_email(email)
+        new = Contact.create(contact_params(email)) if existing.nil?
+        params[:message][:recipient_ids] << (existing.nil? ? new.id : existing.id)
       end
-      params[:message][:recipient_ids] = recip_ids
     end
 end
