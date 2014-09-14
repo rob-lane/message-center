8db7dafbc4eb9aab4c47ff919887c001741edf1d
roblane09 <roblane09@gmail.com>
2014-09-13 17:48:07 -0700

    adding created and updated fields to ember message model

--- app/assets/javascripts/models/message.js
+++ app/assets/javascripts/models/message.js
@@ -1,4 +1,6 @@
 MessageCenter.Message = DS.Model.extend({
   subject: DS.attr('string'),
-  body: DS.attr('string')
+  body: DS.attr('string'),
+  created_at: DS.attr('string'),
+  updated_at: DS.attr('string')
 });
