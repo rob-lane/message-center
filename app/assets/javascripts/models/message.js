5ca102354ed7e2f517d9de7c45db224b877e6793
r.lane <r.lane@fluidappsdev.com>
2014-09-14 15:53:54 -0700

    recipients format validator for recipients

--- app/assets/javascripts/models/message.js
+++ app/assets/javascripts/models/message.js
@@ -5,6 +5,9 @@ MessageCenter.Message = DS.Model.extend(Ember.Validations.Mixin, {
   updated_at: DS.attr('string'),
   recipients: DS.attr('list'),
   validations: {
+    recipients: {
+      format: /\b\w+@[\w.]+\.[A-z.]{2,4}\b/
+    },
     body: {
       presence: { message: 'A message body is required...' }
     }
