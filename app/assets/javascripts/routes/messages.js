f776458d4b3b7da5f0be1a28738feb2001f9a121
roblane09 <roblane09@gmail.com>
2014-09-14 01:40:56 -0700

    semicolin insertion

--- app/assets/javascripts/routes/messages.js
+++ app/assets/javascripts/routes/messages.js
@@ -1,7 +1,7 @@
 // New message route
 MessageCenter.MessagesNewRoute = Ember.Route.extend({
   model: function() {
-    return this.store.createRecord('message')
+    return this.store.createRecord('message');
   },
   setupController: function(controller, message) {
     controller.set('model', message);
