e076935302fed4e4fe542b635c46310e1a3eef9d
r.lane <r.lane@fluidappsdev.com>
2014-09-14 15:02:52 -0700

    recipients to front-end model and API output

--- app/assets/javascripts/models/message.js
+++ app/assets/javascripts/models/message.js
@@ -3,9 +3,19 @@ MessageCenter.Message = DS.Model.extend(Ember.Validations.Mixin, {
   body: DS.attr('string'),
   created_at: DS.attr('string'),
   updated_at: DS.attr('string'),
+  recipients: DS.attr('list'),
   validations: {
     body: {
       presence: { message: 'A message body is required...' }
     }
+  }
+});
+
+MessageCenter.ListTransform = DS.Transform.extend({
+  deserialize: function(serialized) {
+    return serialized.join(',');
+  },
+  serialize: function(deserialized) {
+    return deserialized.split(',');
   }
 });
