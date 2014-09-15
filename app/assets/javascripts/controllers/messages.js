db2a8a8641d41bb98e099e7e792116f49ee468aa
roblane09 <roblane09@gmail.com>
2014-09-14 21:39:38 -0700

    fixing html syntax issue, sorting working

--- app/assets/javascripts/controllers/messages.js
+++ app/assets/javascripts/controllers/messages.js
@@ -1,14 +1,8 @@
 MessageCenter.MessagesController = Ember.ArrayController.extend({
   actions: {
     sort: function(sort_field) {
-      if (this.get('isSorted')) {
-        if (this.get('sortProperties').indexOf(sort_field) < 0) {
-          this.toggleProperty('sortAscending');
-        }
-      } else {
-        this.set('sortProperties', [sort_field])
-        this.set('isSorted', true)
-      }
+      this.set('sortProperties', [sort_field]);
+      this.toggleProperty('sortAscending');
     }
   }
 });
