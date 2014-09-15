c4f9a3b4e1405ece47b87e966a0c75b9a330e83e
r.lane <r.lane@fluidappsdev.com>
2014-09-14 18:40:32 -0700

    typo in migration

--- db/migrate/20140915010843_create_link.rb
+++ db/migrate/20140915010843_create_link.rb
@@ -1,7 +1,7 @@
 class CreateLink < ActiveRecord::Migration
   def change
     create_table :links do |t|
-      t.int, :message_id
+      t.integer :message_id
       t.string :url
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140914095103) do
+ActiveRecord::Schema.define(version: 20140915010843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 20140914095103) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "links", force: true do |t|
+    t.integer "message_id"
+    t.string  "url"
   end
 
   create_table "messages", force: true do |t|
