14354097f080b4cdce8c7a534ff0ba4f637e43e3
roblane09 <roblane09@gmail.com>
2014-09-15 12:29:14 -0700

    Adding user->message association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :email, presence: true,
+            format: /[A-Za-z\d._%+-]+@[A-Za-z\d.-]+\.[A-Za-z]{2,4}/
+
+  has_many :messages
 end
--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -5,11 +5,13 @@ FactoryGirl.define do
     subject "MyString"
     body "MyString"
     recipients {[FactoryGirl.create(:contact)]}
+    user
   end
 
   factory :message_with_links, class: :message do
     subject "A test email"
     body "Checkout http://www.google.com or http://www.yahoo.com"
     recipients {[FactoryGirl.create(:contact, email: 'rspec@tester.com')]}
+    user
   end
 end
